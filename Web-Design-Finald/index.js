3a9b0f646b2d096890db7ef3ea33104c9e4abe57
Your Name <ronald.alean@ndsu.edu>
2022-04-23 18:18:51 -0400

    Commt

---
+++ Web-Design-Finald/index.js
@@ -0,0 +1,11 @@
+/*
+window.addEventListener('load', function(){
+    var newVideo = document.getElementById('videoElementId');
+    newVideo.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+
+    newVideo.play();
+
+}); */
